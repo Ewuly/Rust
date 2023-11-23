4a7e18fe463dd36ab29155cc063e02ec17fb74a1
Ewan <ewan.hamon@gmail.com>
2023-11-23 18:10:28 +0100

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,43 @@
-fn largest<T>(list: &[T]) -> &T {
-    let mut largest = &list[0];
-
-    for item in list {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
+struct Triangle {
+    base: u32,
+    side: u32,
 }
 
-//main
+impl Triangle {
+    fn calculate_perimetre(&self) -> u32 {
+        self.base + (2 * self.side)
+    }
+
+    fn is_base_greater_than_side(&self) -> bool {
+        self.base > self.side
+    }
+
+    fn is_triangle_bigger(&self, other: &Triangle) -> bool {
+        self.calculate_perimetre() > other.calculate_perimetre()
+    }
+}
+
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
+    // Triangle isocèle: base + (2* côté)
+    //x = base, y = c$oté
+    let first_tirangle = Triangle {
+        base: 15,
+        side: 12
+    };
 
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
+    let second_tirangle = Triangle {
+        base: 22,
+        side: 25
+    };
 
-    let char_list = vec!['y', 'm', 'a', 'q'];
+    println!(
+        "Notre first_tirangle: Le périmètre d'un first_tirangle isocèle de base {}cm et de côté {}cm est égal à: {}cm ",
+        first_tirangle.base, first_tirangle.side, first_tirangle.calculate_perimetre()
+    );
 
-    let result = largest(&char_list);
-    println!("The largest char is {}", result);
+    println!("La base est plus grande que les côtés: {}", first_tirangle.is_base_greater_than_side());
+    println!("La base est plus grande que les côtés: {}", second_tirangle.is_base_greater_than_side());
+
+    println!("Est ce que le 1er triangle est plus grand que le 2eme: {} (1er/2eme triangle{} > {})",
+    first_tirangle.is_triangle_bigger(&second_tirangle), first_tirangle.calculate_perimetre(), second_tirangle.calculate_perimetre());
 }
