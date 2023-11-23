f1efb6238d6563d4ac28e2dc800664382c087f1f
Ewan <ewan.hamon@gmail.com>
2023-11-23 17:14:12 +0100

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
-use std::net::TcpListener;
+fn largest<T>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let number_list = vec![34, 50, 25, 100, 65];
 
-    for stream in listener.incoming() {
-        let _stream = stream.unwrap();
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
 
-        println!("Connection established!");
-    }
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
 }
